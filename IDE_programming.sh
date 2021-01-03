972b7c664a21966fa8e05f7923cc0ab63774432a
Marianolara13 <71089355+Marianolara13@users.noreply.github.com>
2021-01-03 10:45:28 -0700

    Add files via upload

---
+++ IDE_programming.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#-*- ENCONDING: UTF-8 -*-
+#instalacion de IDE para diferentes lenguajes y algunos complementos
+
+sudo pacman -S netbeans
+sudo pacman -S intellij-idea-community-edition
+sudo pacman -S mariadb 
+sudo pacman -S mysql-workbench
+sudo pacman -S pycharm-community-edition
+
